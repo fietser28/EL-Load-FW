df669001af07b75d4fc9598c7ab6dff377c5f81b
Jan Nieuwstad <jan.sources@nieuwstad.net>
2023-12-01 17:15:22 +0100

    Added preliminairy MCP23017 library

---
+++ src/gpio_mcp23017.cpp
@@ -0,0 +1,130 @@
+// SPDX-FileCopyrightText: 2023 Jan Nieuwstad <jan.sources@nieuwstad.net>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+
+#include "gpio_mcp23017.h"
+
+#define MCP23017_SPEED 400000
+
+void gpio_mcp23017::begin(TwoWire *Wire, SemaphoreHandle_t WireSemaphore, uint8_t Address)
+{
+    _wire = Wire;
+    _sem = WireSemaphore;
+    _addr = Address;
+    reset();
+};
+
+void gpio_mcp23017::reset()
+{
+    // First Set bank mode to 1 and combine interrupt pins
+    writeI2C(MCP23X17_ADDR_IOCON, 0xC0);
+    _regs[0][MCP23X17_ADDR_IOCON] = 0xC0;
+    _regs[1][MCP23X17_ADDR_IOCON] = 0xC0;
+
+    // Set default POR/RST values (in case reset pin is not connected/used).
+    writeI2C(MCP23X17_BANKA | MCP23X17_ADDR_IODIR, 0xff);
+    writeI2C(MCP23X17_BANKB | MCP23X17_ADDR_IODIR, 0xff);
+    // All other registers default to 0x00, except for IOCON!
+    // Not the fastest way but it works.
+    for (int i = 1; i <= MCP23X17_ADDR_LAST; i++)
+    {
+        // Skip IOCON
+        if (i != MCP23X17_ADDR_IOCON) {
+            writeI2C(MCP23X17_BANKA | i, 0x00);
+            _regs[0][i] = 0x00;
+            writeI2C(MCP23X17_BANKB | i, 0x00);
+            _regs[1][i] = 0x00;
+        }
+    }
+};
+
+uint8_t gpio_mcp23017::digitalRead(uint8_t bank)
+{
+    return readI2C(bank | MCP23X17_ADDR_GPIO);
+};
+
+uint8_t gpio_mcp23017::interruptFlagged(uint8_t bank)
+{
+    return readI2C(bank | MCP23X17_ADDR_INTF);
+};
+
+uint8_t gpio_mcp23017::digitalWrites(uint8_t bank, uint8_t values)
+{
+    return writeI2C(bank | MCP23X17_ADDR_OLAT, values);
+}
+
+uint8_t gpio_mcp23017::digitalWrite(uint8_t bank, uint8_t pin, bool value)
+{
+    return digitalWrites(bank, (_regs[bank == MCP23X17_BANKB][MCP23X17_ADDR_OLAT] &  ~(1 << pin)) | (value << pin));
+}
+
+uint8_t gpio_mcp23017::pinModes(uint8_t bank, uint8_t pins)
+{
+    return writeI2C(bank | MCP23X17_ADDR_IODIR, pins);
+}
+
+uint8_t gpio_mcp23017::pinMode(uint8_t bank, uint8_t pin, bool input) 
+{
+    return pinModes(bank, _regs[bank == MCP23X17_BANKB][MCP23X17_ADDR_IODIR] | (input << pin));
+}
+
+uint8_t gpio_mcp23017::pinInterrupts(uint8_t bank, uint8_t enable, uint8_t onValue, uint8_t compValue)
+{
+    uint8_t result = writeI2C(bank | MCP23X17_ADDR_DEFVAL, compValue);
+    result = result || writeI2C(bank | MCP23X17_ADDR_INTCON, onValue);
+    return result || writeI2C(bank | MCP23X17_ADDR_GPINTEN, enable);
+}
+
+uint8_t gpio_mcp23017::pinInterrupt(uint8_t bank, uint8_t pin, bool enable, bool onValue, bool compValue)
+{
+    return pinInterrupts(bank, _regs[bank == MCP23X17_BANKB][MCP23X17_ADDR_GPINTEN] | (enable << pin), 
+                         _regs[bank == MCP23X17_BANKB][MCP23X17_ADDR_IOCON] | (onValue << pin), 
+                         _regs[bank == MCP23X17_BANKB][MCP23X17_ADDR_DEFVAL] | ( compValue << pin));
+}
+
+uint8_t gpio_mcp23017::writeI2C(uint8_t opcode, uint8_t data)
+{
+    if (_sem != NULL)
+    {
+        if (xSemaphoreTake(_sem, (TickType_t)100) == pdTRUE)
+        {
+            _wire->setClock(MCP23017_SPEED);
+            _wire->beginTransmission(_addr);
+            _wire->write(opcode);
+            _wire->write(data);
+            uint8_t result = _wire->endTransmission(true);
+            xSemaphoreGive(_sem);
+            if (result == 0) {
+                _regs[(opcode & 0x10) == MCP23X17_BANKB][opcode] = data;
+            }
+            return result;
+        }
+        return 5; // timeout. Arduino-ish convention
+    }
+    return 4; // Arduino-ish convention
+}
+
+uint8_t gpio_mcp23017::readI2C(uint8_t opcode)
+{
+    if (_sem != NULL)
+    {
+        if (xSemaphoreTake(_sem, (TickType_t)100) == pdTRUE)
+        {
+            _wire->setClock(MCP23017_SPEED);
+            _wire->beginTransmission(_addr);
+            _wire->write(opcode);
+            _wire->endTransmission();
+            _wire->requestFrom(_addr, 1);
+            uint8_t result = _wire->read();
+            xSemaphoreGive(_sem);
+            _regs[(opcode & 0x10) == MCP23X17_BANKB][opcode] = result;
+            return result;
+        }
+    }
+    return 0; // TODO: proper error handling?
+}
---
+++ src/gpio_mcp23017.h
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: 2023 Jan Nieuwstad <jan.sources@nieuwstad.net>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+//#include <task.h>
+
+#define MCP23X17_ADDR_IODIR 0x00
+#define MCP23X17_ADDR_IPOL 0x01
+#define MCP23X17_ADDR_GPINTEN 0x02
+#define MCP23X17_ADDR_DEFVAL 0x03
+#define MCP23X17_ADDR_INTCON 0x04
+#define MCP23X17_ADDR_IOCON 0x05
+#define MCP23X17_ADDR_GPPU 0x06
+#define MCP23X17_ADDR_INTF 0x07
+#define MCP23X17_ADDR_INTCAP 0x08
+#define MCP23X17_ADDR_GPIO 0x09
+#define MCP23X17_ADDR_OLAT 0x0A
+
+#define MCP23X17_ADDR_LAST 0x0A
+
+#define MCP23X17_BANKA 0x00
+#define MCP23X17_BANKB 0x10
+
+class gpio_mcp23017
+{
+    public:
+        gpio_mcp23017() {}; 
+        void begin(TwoWire *Wire, SemaphoreHandle_t WireSemaphore, uint8_t Address);
+        void reset();
+        uint8_t digitalRead(uint8_t bank);
+        uint8_t interruptFlagged(uint8_t bank);
+        uint8_t digitalWrites(uint8_t bank, uint8_t values);
+        uint8_t digitalWrite(uint8_t bank, uint8_t pin, bool value);
+        uint8_t pinModes(uint8_t bank, uint8_t pins);
+        uint8_t pinMode(uint8_t bank, uint8_t pin, bool mode);
+        uint8_t pinInterrupts(uint8_t bank, uint8_t enable, uint8_t onValue, uint8_t compValue);
+        uint8_t pinInterrupt(uint8_t bank, uint8_t pin, bool enable, bool onValue, bool compValue);
+
+    private:
+        TwoWire *_wire;
+        uint8_t _addr;
+        SemaphoreHandle_t _sem;
+
+        // chip registers 
+        uint8_t _regs[2][MCP23X17_ADDR_LAST];
+
+        uint8_t readI2C(uint8_t opcode);
+        uint8_t writeI2C(uint8_t opcode, uint8_t data);
+
+};
+
